fe2d8e64b870d05c28f065e49d4e54a5cf18b37e
srushtianakrani <123862446+srushtianakrani@users.noreply.github.com>
2024-06-29 16:44:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const connectDB = async () => {
@@ -15,5 +14,4 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
 module.exports = connectDB;
